655e2fd87d3525938e5d94fa5b126a7f3238df2d
Reagan Brasch <reaganbrasch@gmail.com>
2020-09-14 13:32:38 +1000

    ex 4-11

--- chapter4/polish-calc2/getop.c
+++ chapter4/polish-calc2/getop.c
@@ -2,24 +2,43 @@
 #include <ctype.h>
 #include "calc.h"
 
+/*
+    Modify getop so that it doesn't need to use ungetch. Hint:
+    use an internal static variable.
+*/
+
 int getop(char s[])
 {
-    int i, c;
- 
-    while ((s[0] = c = getch()) == ' ' || c == '\t')
-        ;
+    int i;
+    static int c = ' ';
+
+    while (c == ' ' || c == '\t')
+        c = getchar();
+
+    s[0] = c;
     s[1] = '\0';
+
     if (!isdigit(c) && c != '.')
-        return c; /* not a number */
+    {
+        int res = c;
+        c = ' ';
+        return res; /* not a number */
+    }
+
     i = 0;
+
     if (isdigit(c)) /* collect integer part */
         while (isdigit(s[++i] = c = getch()))
             ;
+
     if (c == '.') /* collect fraction part */
         while (isdigit(s[++i] = c = getch()))
             ;
+
     s[i] = '\0';
+
     if (c != EOF)
         ungetch(c);
+
     return NUMBER;
 }
